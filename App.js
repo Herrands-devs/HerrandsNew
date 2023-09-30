cdb2844998e443c83f2babbdb63fe5e3968c1d42
unknown <Kelly Owoju>
2023-09-30 10:09:33 +0100

    Project setup v1

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    MontserratRegular: require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className={`flex-1 justify-center items-center`}>
+      <Text className={`text-[30px] font-montserratRegular text-center`}>
+        Welcome to Herrand Customer App
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
