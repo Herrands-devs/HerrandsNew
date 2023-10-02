0b9d58064ba468f78228676385494138fe9ee7bb
unknown <Kelly Owoju>
2023-10-02 13:33:24 +0100

    created the customer sidebar

--- App.js
+++ App.js
@@ -8,14 +8,17 @@ import {
   SquareButton,
 } from "./screens/components/common/Button";
 import { colors } from "./themes/colors";
+import Sidebar from "./screens/components/customer-home-screen/Sidebar";
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     async function loadCustomFonts() {
       await Font.loadAsync({
         MontserratRegular: require("./assets/fonts/Montserrat-Regular.ttf"),
+        MontserratBold: require("./assets/fonts/Montserrat-Bold.ttf"),
       });
       setFontLoaded(true);
     }
@@ -27,13 +30,21 @@ export default function App() {
     return null;
   }
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <View className={`flex-1 justify-center items-center`}>
       <Text className={`text-[30px] font-montserratRegular text-center`}>
         Welcome to Herrand Customer App
       </Text>
 
-      <RoundedButton text={"Rounded Button"} />
+      <RoundedButton text={"Open Sidebar"} onPress={toggleSidebar} />
       <SquareButton
         text={"Square Button"}
         styles={{ backgroundColor: "#0066F5" }}
@@ -59,6 +70,7 @@ export default function App() {
       />
       <DisabledSquareBtn text={"Square Disabled"} />
       <DisabledRoundedBtn text={"Rounded Disabled"} />
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
     </View>
   );
 }
--- screens/components/common/Button.jsx
+++ screens/components/common/Button.jsx
@@ -1,12 +1,13 @@
 import { TouchableOpacity, Text } from "react-native";
 
-export const RoundedButton = ({ text }) => {
+export const RoundedButton = ({ text, onPress }) => {
   return (
     <TouchableOpacity
       className={`mt-[30px] bg-primaryColor w-[80%] 
       p-[12px] flex flex-row justify-center rounded-[40px]`}
+      onPress={onPress}
     >
-      <Text className={`text-white text-[14px] font-semibold`}>{text}</Text>
+      <Text className={`text-white text-[14px] font-semibold font-montserratRegular`}>{text}</Text>
     </TouchableOpacity>
   );
 };
@@ -18,7 +19,11 @@ export const SquareButton = ({ text, styles }) => {
       p-[12px] flex flex-row justify-center rounded-[4px]`}
       style={styles}
     >
-      <Text className={`text-white text-[14px] font-semibold`}>{text}</Text>
+      <Text
+        className={`text-white text-[14px] font-semibold font-montserratRegular`}
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -30,7 +35,11 @@ export const DisabledSquareBtn = ({ text, styles }) => {
       p-[12px] flex flex-row justify-center rounded-[4px] bg-disabledBtn`}
       style={styles}
     >
-      <Text className={`text-white text-[14px] font-semibold`}>{text}</Text>
+      <Text
+        className={`text-white text-[14px] font-semibold font-montserratRegular`}
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -42,7 +51,11 @@ export const DisabledRoundedBtn = ({ text, styles }) => {
       p-[12px] flex flex-row justify-center rounded-[40px] bg-disabledBtn`}
       style={styles}
     >
-      <Text className={`text-white text-[14px] font-semibold`}>{text}</Text>
+      <Text
+        className={`text-white text-[14px] font-semibold font-montserratRegular`}
+      >
+        {text}
+      </Text>
     </TouchableOpacity>
   );
 };
